dcb137004f2bf6dcc5fd9f66a07d440a23f2421b
Mair-Jun <lluxuryl98@naver.com>
2020-11-25 20:41:08 +0900

    가로등 추가

---
+++ Project/Source/InGame/Actor/Tile/LightTile.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Tile.h"
+
+class LightTile : public Tile
+{
+public:
+	LightTile(const std::weak_ptr<class Scene>& scene, Type type = Type::Light);
+	virtual ~LightTile() noexcept;
+
+	virtual void initailize() override;
+
+	virtual void updateActor(float deltatime) override;
+
+private:
+	std::shared_ptr<class Actor> mLight;
+};
