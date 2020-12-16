7271ab4a816b9dd29696dffb4f2b5d4c93f4b940
Mair-Jun <lluxuryl98@naver.com>
2020-12-16 10:30:57 +0900

    Update

--- Project/Source/InGame/Scene/GameScene.cpp
+++ Project/Source/InGame/Scene/GameScene.cpp
@@ -15,10 +15,7 @@
 #include "../../Game/Actor/Camera/CameraActor.h"
 #include "../../Game/Actor/Camera/FollowCameraActor.h"
 #include "../../Game/Sound/Sound.h"
-
-//#include "../Minimap/GameMinimap.h"
 #include "../Minimap/GameMinimap.h"
-
 #include "../Actor/Player/Type/ControlPlayer.h"
 #include "../Actor/Particle/ParticleCreator.h"
 #include "../Actor/Player/Type/MinionAi/MinionManager.h"
